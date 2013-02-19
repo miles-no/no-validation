f728a0cf97febcf1af8e12872cfa9c7638fec4ec
jorgen <jorgen@jorgenjohansen.com>
2013-02-19 08:46:46 +0100

    Fixed a few issues in code

--- noValidation.js
+++ noValidation.js
@@ -1,15 +1,16 @@
 module.exports = (function(){
-    var _mod11OfNumberWithControlDigit = function (input) {
+    "use strict";
+    var mod11OfNumberWithControlDigit = function (input) {
         var controlNumber = 2,
             sumForMod = 0,
-            i;
+            i, result;
 
         for (i = input.length - 2; i >= 0; --i) {
             sumForMod += input.charAt(i) * controlNumber;
             if (++controlNumber > 7)
                 controlNumber = 2;
         }
-        var result = (11 - sumForMod % 11);
+        result = (11 - sumForMod % 11);
 
         return result === 11 ? 0 : result;
     };
@@ -23,7 +24,7 @@ module.exports = (function(){
             return false;
         }
         return parseInt(accountNumber.charAt(accountNumber.length - 1), 10) 
-                === _mod11OfNumberWithControlDigit(accountNumber);
+                === mod11OfNumberWithControlDigit(accountNumber);
     };
 
     var organizationNumber = function(orgNumber){
@@ -32,7 +33,7 @@ module.exports = (function(){
             return false;
         }
         return parseInt(orgNumber.charAt(orgNumber.length - 1), 10) 
-            === _mod11OfNumberWithControlDigit(orgNumber);
+            === mod11OfNumberWithControlDigit(orgNumber);
     };
 
     var birthNumber = function(birthNumber){
@@ -41,39 +42,39 @@ module.exports = (function(){
             return false;
         }
         
-        var _sum = function(birthNumber, factors){
+        var sum = function(number, factors){
             var sum = 0;
-            for(i = 0, l = factors.length; i < l; ++i){
-                sum += parseInt(birthNumber.charAt(i),10) * factors[i];
+            for(var i = 0, l = factors.length; i < l; ++i){
+                sum += parseInt(number.charAt(i),10) * factors[i];
             }
             return sum;
         };
 
-        var checksum1 = 11 - (_sum(birthNumber, [3, 7, 6, 1, 8, 9, 4, 5, 2]) % 11);
-        var checksum2 = 11 - (_sum(birthNumber, [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]) % 11);
+        var checksum1 = 11 - (sum(birthNumber, [3, 7, 6, 1, 8, 9, 4, 5, 2]) % 11);
+        var checksum2 = 11 - (sum(birthNumber, [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]) % 11);
         return checksum1 === parseInt(birthNumber.charAt(9), 10) 
                 && checksum2 === parseInt(birthNumber.charAt(10), 10);
     };
 
     var kidNumber = function(kidNumber){
-        var kidNumber = kidNumber.toString();
+        kidNumber = kidNumber.toString();
         var controlDigit = kidNumber.charAt(kidNumber.length - 1);
-        return parseInt(controlDigit, 10) === _mod11OfNumberWithControlDigit(kidNumber)
-                || parseInt(controlDigit, 10) === _luhnValue(kidNumber);
+        return parseInt(controlDigit, 10) === mod11OfNumberWithControlDigit(kidNumber)
+                || parseInt(controlDigit, 10) === luhnValue(kidNumber);
     };
 
-    var _luhnValue = function(number){
-        var sum = 0, dbl = 0;
-        for(var i = number.length-2; i >= 0; i-=2){
+    var luhnValue = function(number){
+        var sum = 0, dbl = 0, i;
+        for(i = number.length-2; i >= 0; i-=2){
             dbl = (parseInt(number.charAt(i), 10) * 2).toString();
             sum += parseInt(dbl.charAt(0), 10) + parseInt(dbl.charAt(1) || 0, 10);
         }
-        for(var i = number.length -3; i >= 0; i-=2){
+        for(i = number.length -3; i >= 0; i-=2){
             sum += parseInt(number.charAt(i), 10);
         }
         sum = sum.toString();
         return 10 - parseInt(sum.charAt(sum.length -1), 10);
-    }
+    };
 
     return {
         accountNumber: accountNumber,
